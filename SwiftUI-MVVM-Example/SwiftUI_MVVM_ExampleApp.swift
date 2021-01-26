a5de67b2e5dc487fe3e3a452ef94e26cc3951733
zakbarlow1995 <zak@stormideas.com>
2021-01-26 10:46:28 +0000

    Initial Commit

---
+++ SwiftUI-MVVM-Example/SwiftUI_MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ExampleApp.swift
+//  SwiftUI-MVVM-Example
+//
+//  Created by Zak Barlow on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
